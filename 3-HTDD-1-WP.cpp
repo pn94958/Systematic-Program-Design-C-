81e239eda6929bdb8c3294e2e5f7a77b2ca6a0ca
CodingRodent <pn94958@users.noreply.github.com>
2021-03-10 02:14:19 +0100

    Update 3-HTDD-1-WP.cpp
    Somewhat sloppy. I'm still not sure how to apply to formula to C++. Just wanted to get the program working.

--- 3-HTDD-1-WP.cpp
+++ 3-HTDD-1-WP.cpp
@@ -40,27 +40,70 @@ bool CheckExpect(int a, int b)
     return check;
 }
 
+bool CheckExpect2(char a, char b)
+{
+    bool check;
+    if(a == b)
+    {
+        cout << "Test passed successfully.\n";
+        check = true;
+    }
+    else
+    {
+        cout << "The result is expected to be " << b << " but is " << a << ".\n";
+        check = true;
+    }
+    
+    return check;
+}
+
 //Monkey -> Monkey
-//produce next monkey incread by 1
+//produce next monkey increased by 1 until it reaches 6
 // int NextMonkey(int mk) { return 0; }   //stub
 
 //<use template from Monkey>
 
 int NextMonkey(int mk)
 {
-    if(mk>5)
+    if(mk<6)
     {
         mk++;
     }
     return mk;
 }
 
+//Monkey Number -> Char
+//produce '@' if consumed number is equal to monkey and else produce '.'
+
+//char RenderMonkey(int mk, int n) { return 'a'; }    //stub
+
+//<use template from Monkey>
+
+char RenderMonkey(int mk, int n)
+{
+    char rm;
+    if(mk == n)
+    { rm = '@'; }
+    else
+    { rm = '.'; }
+    return rm;
+}
+
 // Monkey -> Monkey
-// start the world with mk
-// 
+// Moves monkey from left to right after every keypress
 int main()
 {
-    CheckExpect(NextMonkey(mk),0);
-    CheckExpect(NextMonkey(5),5); 
+    //CheckExpect(NextMonkey(mk),0);
+    //CheckExpect(NextMonkey(5),5); 
+    //CheckExpect2(RenderMonkey(5,5),'@');
+    //CheckExpect2(RenderMonkey(1,5),'.');
+    cout << "Watch the monkey move left to right after pressing enter!" << endl;
+    while(mk < 6)
+    {
+    cout << RenderMonkey(mk,0) << RenderMonkey(mk,1) << RenderMonkey(mk,2) << RenderMonkey(mk,3) << RenderMonkey(mk,4) << endl;
+    mk = NextMonkey(mk);
+    cin.get();
+    }
+
     return 0;
 }
